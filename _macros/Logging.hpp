e23730b4e58c65359a1c4e8f6a97e40f3105431f
fatpound <84704675+fatpound@users.noreply.github.com>
2025-11-03 01:29:59 +0300

    Comment out MP_LOG macro in Logging.hpp
    The MP_LOG macro definition has been commented out in both the enabled and disabled sections because its not correctly implemented

--- _macros/Logging.hpp
+++ _macros/Logging.hpp
@@ -7,19 +7,19 @@
 
 #define MP_PRINT(   msg, ...) g_print(   msg __VA_OPT__(,) __VA_ARGS__)
 #define MP_PRINTERR(msg, ...) g_printerr(msg __VA_OPT__(,) __VA_ARGS__)
-#define MP_LOG(     msg, ...) g_log(     msg __VA_OPT__(,) __VA_ARGS__)
 #define MP_LOGMSG(  msg, ...) g_message( msg __VA_OPT__(,) __VA_ARGS__)
 #define MP_LOGWARN( msg, ...) g_warning( msg __VA_OPT__(,) __VA_ARGS__)
 #define MP_LOGERROR(msg, ...) g_error(   msg __VA_OPT__(,) __VA_ARGS__)
+// #define MP_LOG(  msg, ...) g_log(     msg __VA_OPT__(,) __VA_ARGS__)
 
 #else
 
 #define MP_PRINT(   msg, ...)
 #define MP_PRINTERR(msg, ...)
-#define MP_LOG(     msg, ...)
 #define MP_LOGMSG(  msg, ...)
 #define MP_LOGWARN( msg, ...)
 #define MP_LOGERROR(msg, ...)
+// #define MP_LOG(     msg, ...)
 
 #endif
 
